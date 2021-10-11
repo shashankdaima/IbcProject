7a59460071f2aed8f3d35d4659f15528f419bcc8
shashankdaima <daimashashank10@gmail.com>
2021-10-11 15:27:20 +0530

    minor:class implementation in studentpostpage

--- client/src/pages/StudentPostAnswerPage.js
+++ client/src/pages/StudentPostAnswerPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-function StudentPostAnswerPage() {
-    return (
-        <div>
-            <h1>This will be the page where student can upload their answer sheets.</h1>
-        </div>
+import {
+  Navbar,
+  Container
+} from 'reactstrap';
+class StudentPostAnswerPage extends Component {
 
-    )
-}
+    render() {
+      return (
+        <div  >
+            
+        </div>
+      );
+    }
+  }
 export default StudentPostAnswerPage;
