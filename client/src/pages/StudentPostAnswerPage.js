d285e7ae83269ed9daad175d3e22bf45e82f136b
shashankdaima <daimashashank10@gmail.com>
2021-10-21 20:49:10 +0530

    minor

--- client/src/pages/StudentPostAnswerPage.js
+++ client/src/pages/StudentPostAnswerPage.js
@@ -1,11 +1,69 @@
 import React, { Component } from "react";
 import { Container, Card, Form, Button } from "react-bootstrap"
+import axios from 'axios'
 
 class StudentPostAnswerPage extends Component {
+  state = {
+    //No file
+    selectedFile: null
+  };
+  onFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+  onFileUpload = () => {
+
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    axios.post("api/uploadfile", formData);
+  };
+  fileData = () => {
+
+    if (this.state.selectedFile) {
+
+      return (
+        <div>
+          <h2>File Details:</h2>
+
+          <p>File Name: {this.state.selectedFile.name}</p>
+
+
+          <p>File Type: {this.state.selectedFile.type}</p>
+
+
+          <p>
+            Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
+
   render() {
 
     function answerPostOnClicked(params) {
-        
+
     }
     return (
       <Container >
@@ -21,7 +79,7 @@ class StudentPostAnswerPage extends Component {
               </Form.Group>
               <Form.Group className="mb-3" >
                 <Form.Label >Answer Sheet</Form.Label>
-                <Button variant="outline-primary" className="m-3" onClick="answerPostOnClicked">
+                <Button variant="outline-primary" className="m-3" onClick={answerPostOnClicked} >
                   Attach
                 </Button>
                 <br />
