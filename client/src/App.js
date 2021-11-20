e71e76f2a92a78774ae89c356f348ddf177c9967
shashankdaima <daimashashank10@gmail.com>
2021-11-20 18:48:53 +0530

    commented out web3js

--- client/src/App.js
+++ client/src/App.js
@@ -32,23 +32,23 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      // // Get network provider and web3 instance.
+      // const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      // // Use web3 to get the user's accounts.
+      // const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      // // Get the contract instance.
+      // const networkId = await web3.eth.net.getId();
+      // const deployedNetwork = SimpleStorageContract.networks[networkId];
+      // const instance = new web3.eth.Contract(
+      //   SimpleStorageContract.abi,
+      //   deployedNetwork && deployedNetwork.address,
+      // );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // // Set web3, accounts, and contract to the state, and then proceed with an
+      // // example of interacting with the contract's methods.
+      // this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -58,25 +58,25 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(7).send({ from: accounts[0] });
+  //   // Stores a given value, 5 by default.
+  //   await contract.methods.set(7).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+  //   // Get the value from the contract to prove it worked.
+  //   const response = await contract.methods.get().call();
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  //   // Update state with the result.
+  //   this.setState({ storageValue: response });
 
-  };
+  // };
 
 
   render() {
 
 
-    if (!this.state.web3) {
+    if (this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
@@ -92,9 +92,7 @@ class App extends Component {
                   <Link to="/result">Result</Link>
                 </Navbar.Text>
                 <p>&nbsp;</p>
-                <Navbar.Text>
-                  <Link to="/result">Result</Link>
-                </Navbar.Text>
+                
               </Navbar.Collapse>
             </Container>
           </Navbar>
